78f74be23f7147c8e8fa042c780ff8e7e3139dd5
Abhishek kumar <aasalex21c@gmail.com>
2024-03-09 17:19:39 +0530

    added react boilerplate code

---
+++ ReactBoilerplateCode/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Assests/fontAwesomeProIcons/fontAwesomeIcons.css';
+
+function App() {
+  console.log(quotes);
+  return (
+    <div>
+      Our React App, is up and running!
+    </div>
+
+  );
+}
+
+export default App;
